c1e42ae26f95fe02c5241611f7944077408dad10
Alonso <acerpas10@gmail.com>
2019-09-03 18:03:58 -0500

    t

--- video_to_frames/main.py
+++ video_to_frames/main.py
@@ -1,15 +1,28 @@
 import cv2
-vidcap = cv2.VideoCapture('GH010005.MP4')
+
+vidcap = cv2.VideoCapture('GH010023.MP4')
 success,image = vidcap.read()
 count = 0
 count_valids = 0
-while count != 840:
-    if success == True:
+fps = 60
+seconds = 37
+step = 30
+count_step = 1
+while count < fps * seconds:
+    if count_step % step == 0:
+        while success == False:
+            success,image = vidcap.read()
+            count += 1
         num_frame = str(count_valids).zfill(4)
         name_frame = "frame" + num_frame + ".jpg"
+        print('Saved frame: ', name_frame)
         cv2.imwrite(name_frame, image)     # save frame as JPEG file
         count_valids += 1
+        count_step = 1
+    else:
+        count_step += 1
+        
     success,image = vidcap.read()
-    print('Read a new frame: ', success)
+    print('Read frame ', count, ': ', success)
     count += 1
     
